9f20e95f8a70b7027260121141585724eb8f11e6
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2019-08-25 22:16:46 +1000

    Added Class Intersection

---
+++ src/Roads/Intersection.java
@@ -0,0 +1,9 @@
+package Roads;
+import Vehicles.Bus;
+
+public class Intersection {
+    boolean isNorth = true;
+    boolean isSouth = true;
+    boolean isEast = true;
+    boolean isWest = true;
+}
