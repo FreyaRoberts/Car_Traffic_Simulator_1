d70a886386ba4b5a9cf709347ea5eec2cd59c194
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2019-08-25 22:00:22 +1000

    Added comments and reformatted motorbike and bus

--- src/Vehicles/Bus.java
+++ src/Vehicles/Bus.java
@@ -2,4 +2,6 @@ package Vehicles;
 
 public class Bus extends Car {
     double len = 3 * length;
+    // Length of a bus is 3 times a car
+
 }
--- src/Vehicles/Motorbike.java
+++ src/Vehicles/Motorbike.java
@@ -1,6 +1,7 @@
 package Vehicles;
 
 public class Motorbike extends Car {
-    double len = 0.5*length;
+    double len = 0.5 * length;
+    // Length of motorbike is half of a car
 
 }
