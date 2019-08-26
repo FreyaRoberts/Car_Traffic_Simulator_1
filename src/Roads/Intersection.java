5f3fb1c184f51fbcb6d15805307d7b0658516ac8
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2019-08-26 10:31:51 +1000

    Added constructor for Intersection Added originatesFrom variable Added setExit method

--- src/Roads/Intersection.java
+++ src/Roads/Intersection.java
@@ -1,9 +1,35 @@
 package Roads;
-import Vehicles.Bus;
 
 public class Intersection {
     boolean hasNorthExit = true;
     boolean hasSouthExit = true;
     boolean hasEastExit = true;
     boolean hasWestExit = true;
+    String originatesFrom;
+
+    public Intersection(String originatesFrom){
+        setOriginatesFrom(originatesFrom);
+        setExits();
+    }
+
+    public void setOriginatesFrom(String originatesFrom) {
+        this.originatesFrom = originatesFrom;
+    }
+
+    public String getOriginatesFrom() {
+        return originatesFrom;
+    }
+
+    public void setExits() {
+        switch (originatesFrom) {
+            case "North":
+                this.hasNorthExit = false;
+            case "South":
+                this.hasSouthExit = false;
+            case "East":
+                this.hasEastExit = false;
+            case "West":
+                this.hasWestExit = false;
+        }
+    }
 }
