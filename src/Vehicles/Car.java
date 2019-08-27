fbbc5031243d9d2fd0eb563c0e3e5565d0abddba
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2019-08-27 16:13:56 +1000

    Removed Package Functionality Removed Class Drive and DriveTest Added Drive functionality to Car class

--- src/Vehicles/Car.java
+++ src/Vehicles/Car.java
@@ -4,6 +4,8 @@ public class Car {
     private final double CAR_LENGTH = 2;
     public double length = CAR_LENGTH;
     double width = 0.5 * length;
+    int frontPos;
+    int backPos;
 
     public double getLength() {
         return length;
@@ -12,4 +14,26 @@ public class Car {
     public void setLength(double length) {
         this.length = length;
     }
+
+         public void drive() {
+            setFrontPos(frontPos + 1);
+            setBackPos((frontPos) - (int) this.length);
+            // No +1 for back position as frontPos has already been increased by 1
+        }
+
+        public int getFrontPos() {
+            return frontPos;
+        }
+
+        public void setFrontPos(int pos) {
+            this.frontPos = pos;
+        }
+
+        public int getBackPos() {
+            return backPos;
+        }
+
+        public void setBackPos(int pos) {
+            this.backPos = pos;
+        }
 }
--- src/Vehicles/CarTest.java
+++ src/Vehicles/CarTest.java
@@ -6,10 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
     @Test
-    void testDefault(){
+    void testDefault() {
         Car car = new Car();
-        assertEquals(2,car.length);
-        assertEquals(1,car.width);
+        assertEquals(2, car.length);
+        assertEquals(1, car.width);
     }
 
+    @Test
+    void testDrive() {
+        Car car = new Car();
+        assertEquals(0, car.frontPos);
+        assertEquals(0, car.backPos);
+        car.drive();
+        assertEquals(1, car.frontPos);
+        assertEquals(-1, car.backPos);
+    }
 }
