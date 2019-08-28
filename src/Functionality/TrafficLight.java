1a85b9faf13b007cd6517da9cc94acd2cf749acf
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2019-08-28 21:36:14 +1000

    Added TrafficLight Class

---
+++ src/Functionality/TrafficLight.java
@@ -0,0 +1,29 @@
+package Functionality;
+
+public class TrafficLight {
+    private int pos;
+    private boolean isGreen;
+    public final int LIGHT_TIME = 5;
+
+    public TrafficLight(int pos) {
+        setPos(pos);
+    }
+
+    public int getPos() {
+        return pos;
+    }
+
+    private void setPos(int pos) {
+        this.pos = pos;
+    }
+
+    public boolean getIsGreen() {
+        return isGreen;
+    }
+
+    public void setIsGreen(boolean green) {
+        isGreen = green;
+    }
+}
+
+
