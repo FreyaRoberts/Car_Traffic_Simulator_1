3890449d9d739e64d97443eed5135b704171bda1
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2019-09-13 09:02:13 +1000

    Updated Motorbike to be public

--- src/Vehicles/Motorbike.java
+++ src/Vehicles/Motorbike.java
@@ -3,7 +3,7 @@ package Vehicles;
 import Roads.Straight;
 
 class Motorbike extends Car {
-    Motorbike(Straight startingRoad) {
+    public Motorbike(Straight startingRoad) {
         super(startingRoad);
         setLength(0.5 * super.getLength());
     }
